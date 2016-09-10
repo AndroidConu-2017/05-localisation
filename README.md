47232376781b7bbbe2346956118e9390f0ffa438
P Campbell <pcampbell.edu@gmail.com>
2016-09-09 20:06:25 -0400

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# week04-localisation
+# Localisation 
+Sample code using android localisation
+@string/app_name etc
+
---
+++ app/src/main/java/cs518/sample/localisation/MainActivity.java
@@ -0,0 +1,15 @@
+package cs518.sample.localisation;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
